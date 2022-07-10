2e197d1cddaca9e77fe2fc0eec06b5186ba5a2b9
Colmc2966 <colmc2966@gmail.com>
2022-07-10 22:17:39 +0000

    adding function for displaying questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,9 +37,37 @@ var questions = [
         correctAnswer: 'b'
     }
 ];
-
+//link between html & script
 var tableContainer = document.getElementById('table');
 var scoreContainer = document.getElementById('score');
 var goButton = document.getElementById('go');
 
-generateQuiz(questions, tableContainer, scoreContainer, goButton);
+generateTable(questions, tableContainer, scoreContainer, goButton);
+
+//generating quiz
+
+function generateTable(questions, tableContainer) {
+//For output of quiz
+    var output = [];
+    var answers;
+    for (var i=0; i<questions.length;i++) {
+        //So answer starts as blank by default
+        answers = [];
+        //Link to correct answer
+        for(letter in questions[i].answers) {
+            // radio button to select answers
+            answers.push(
+                '<label>'
+                  + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                  + letter + ': '
+                  + questions[i].answers[letter]
+                + '</label>'
+              );
+        }
+        output.push(
+            '<div class="question">' + questions[i].question + '</div>'
+            + '<div class="answers">'+ answers.join('') + '</div>'
+        );
+    }
+
+}
