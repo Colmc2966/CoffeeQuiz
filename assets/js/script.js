c4fce0ab6a49b52d9791e14e8f08854b12b5159b
Colmc2966 <colmc2966@gmail.com>
2022-07-10 21:57:28 +0000

    Adding Questions to script

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+        question: "What is the capital of Ireland?",
+        answers: {
+            a:'Tibet',
+            b:'Melbourne',
+            c:'Dublin'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "Who is the current Prime Minister of the UK?",
+        answers: {
+            a: 'David Cameron',
+            b: 'Boris Johnson',
+            c: 'Teresa May'
+        },
+        correctAnswer: 'b'
+    }
+    {
+        question: "Name the famous Chocolate Brand",
+        answers: {
+            a: 'Cadburys',
+            b: 'Tadburs',
+            c: 'CadBars'
+        },
+        correctAnswer: 'a'
+    }
+    {
+        question: "What is a common household pet",
+        answers: {
+            a: 'Ostrich',
+            b: 'Cat',
+            c: 'Giraffe'
+        },
+        correctAnswer: 'b'
+    }
+]
